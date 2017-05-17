3968e6898e839a31b5607b93a129fafe912bd774
Guille <guillermolopez3@gmail.com>
2017-05-17 20:18:44 -0300

    se agregan el diseño del card, los iconos del menu lateral y la pantalla de seleccion de ciclos

---
+++ app/src/main/java/com/gru/cajaaplicacionestics/view/SeleccionActivity.java
@@ -0,0 +1,39 @@
+package com.gru.cajaaplicacionestics.view;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+
+import com.gru.cajaaplicacionestics.R;
+
+public class SeleccionActivity extends AppCompatActivity {
+
+    Button entrar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_seleccion);
+        showToolbar(getResources().getString(R.string.nombre_app),false);
+
+        entrar= (Button)findViewById(R.id.btnEntrarActivitySeleccion);
+
+
+    }
+
+    public void ActivitySeleccionEntrar(View view)
+    {
+        Intent i = new Intent(this,MainActivity.class);
+        startActivity(i);
+    }
+
+    public void showToolbar(String tittle, boolean upButton)
+    {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(tittle);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(upButton);
+    }
+}
